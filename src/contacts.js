1751671ae0c5ca6eefaf9eadca83e0192a03203c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-01-01 10:20:20 -0600

    more work on UI portion

--- src/contacts.js
+++ src/contacts.js
@@ -8,6 +8,45 @@ let relationshipInput: HTMLInputElement;
 let table: HTMLTableElement;
 
 let document: Document;
+let name: string;
+let relationship: string;
+const contacts = {};
+
+function addContact(): void {
+  const id = contacts.length + 1;
+  const contact = {id, name, relationship};
+  contacts[id] = contact;
+
+  const tr = document.createElement('tr');
+  tr.id = String(id);
+
+  let td = document.createElement('td');
+  td.textContent = name;
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+  td.textContent = relationship;
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+  const span = document.createElement('span');
+  span.className = 'fa fa-trash';
+  span.onclick = deleteContact.bind(null, id);
+  td.appendChild(span);
+  tr.appendChild(td);
+
+  table.appendChild(tr);
+}
+
+function deleteContact(id: string): void {
+  delete contacts[id];
+  const tr = document.getElementById(id);
+  if (tr) {
+    const parent = tr.parentNode;
+    if (parent) parent.removeChild(tr);
+  }
+}
+
 
 function getButtonElement(id) {
   return ((document.getElementById(id): any): HTMLButtonElement);
@@ -26,12 +65,12 @@ export function handleAdd(
   event: Event
 ): void {
   event.preventDefault();
-  //messageDiv.textContent = getGreeting(nameInput.value);
+  addContact();
 }
 
 export function handleInput(): void {
-  const name = nameInput.value;
-  const relationship = relationshipInput.value;
+  name = nameInput.value;
+  relationship = relationshipInput.value;
   addButton.disabled = name.length === 0 || relationship.length === 0;
 }
 
@@ -42,13 +81,12 @@ export function onLoad(doc: Document) {
   relationshipInput = getInputElement('relationship-input');
   addButton = getButtonElement('add-button');
   table = getTableElement('contact-table');
-  console.log('contacts.js onLoad: table =', table);
 
   if (!nameInput) throw new Error('name-input not found');
   if (!relationshipInput) throw new Error('relationship-input not found');
   if (!addButton) throw new Error('addButton not found');
 
-  nameInput.onkeyup = handleInput();
-  relationshipInput.onkeyup = handleInput();
+  nameInput.onkeyup = handleInput;
+  relationshipInput.onkeyup = handleInput;
   addButton.onclick = handleAdd.bind(null, nameInput);
 }
